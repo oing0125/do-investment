ebae2a78df81f29d38066ba3f1ce4c7c51886dd2
oing0125 <oing0125@gmail.com>
2020-11-29 21:45:37 +0900

    Init Project - initialize Spring boot - initialize Vue.js

---
+++ src/main/java/doin/controller/EntryController.java
@@ -0,0 +1,13 @@
+package doin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EntryController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World!";
+    }
+}
